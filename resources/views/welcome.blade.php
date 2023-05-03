8f061346a8e40b842298650a3586070b7a63901a
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-05-03 17:42:27 +0530

    Linking Hyper theme assets.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,37 @@
         <title>Movies Index 2</title>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-        <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+        <!-- Daterangepicker css -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/daterangepicker/daterangepicker.css') }}">
+
+        <!-- Vector Map css -->
+        <link rel="stylesheet" href="{{ asset('assets/vendor/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}">
+
+        <!-- Theme Config Js -->
+        <script src="{{ asset('assets/js/hyper-config.js') }}"></script>
+
+        <!-- Icons css -->
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+        <!-- App css -->
+        <link href="{{ asset('assets/css/app-modern.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
 
     </head>
     <body>
-        <div id="app"></div>
+        <div id="app">
+            <router-view></router-view>
+        </div>
 
         @vite('resources/js/app.js')
+        <!-- Vendor js -->
+        <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
+
+        <!-- App js -->
+        <script src="{{ asset('assets/js/app.min.js') }}"></script>
     </body>
 </html>
