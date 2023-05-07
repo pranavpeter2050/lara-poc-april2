8a88862598dc26ecd772490a5d9134465bc44bd0
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-05-07 20:10:37 +0530

    Movies Api resource route defined.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('/movies', MovieController::class);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('movies', MovieController::class);
