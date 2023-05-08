ca49ca0586d2db4f751ad2253c91b4b57854d058
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-05-08 07:20:19 +0530

    Commented Laravel web route.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{vue_capture?}',function () {
     return view('welcome');
 })->where('vue_capture', '[\/\w\.-]*');
+
+// Route::resource('movies', MovieController::class);
